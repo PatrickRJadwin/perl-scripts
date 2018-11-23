cd1ab728df7773a53249cf80a7fa229e51b50b6a
Patrick Jadwin <prjadwin@pstcc.edu>
2018-11-23 11:28:26 -0500

    Cleaned up words file

--- jadwin_words.pl
+++ jadwin_words.pl
@@ -3,44 +3,34 @@
 use strict;
 use warnings;
 
-my @arrWords;
+# Get input or file redirect
+# Infinite loop to read multiple lines
+my @wordarr;
 for (;;) {
-    my $input = <STDIN>;
-    last if not defined $input;
-    chomp $input;
-    push @arrWords, $input;
-}
-
-my $wordsLength = scalar @arrWords;
-
-my @words;
-my @temp;
-
-for (my $i = 0; $i < $wordsLength; $i++) {
-    @temp = split(" ", $arrWords[$i]);
-    my $tempnum = scalar @temp;
-    for (my $x = 0; $x < $tempnum; $x++) {
-        push @words, $temp[$x];
+    my $inp = <STDIN>;
+    last if not defined $inp;  # break
+    chomp $inp;  # cleanup line
+    my @temp = split(" ", $inp);  # split chars into array
+    for (my $i = 0; $i < scalar @temp; $i++) {
+        $temp[$i] =~ s/[^a-zA-Z0-9]//g;  # strip non alpha-numeric characters
+        push @wordarr, $temp[$i];  # push values into array
     }
 }
 
-my $length = scalar @ARGV;
-
-my @wordCount;
-my $count = 0;
-
-for (my $i = 0; $i < $length; $i++) {
-    for (my $x = 0; $x < $wordsLength; $x++) {
-        if (lc($ARGV[$i]) eq lc($words[$x])) {
+# Compare words to arguments
+my @argcount;
+for (my $i = 0; $i < scalar @ARGV; $i++) {
+    my $count = 0;  # word counter
+    for (my $x = 0; $x < scalar @wordarr; $x++) {
+        if (lc($ARGV[$i]) eq lc($wordarr[$x])) {  # equality check
             $count++;
-        }        
+        }
     }
-    push @wordCount, $count;
-    $count = 0;
-}
-
-my $arrLngth = scalar @wordCount;
-
-for (my $i = 0; $i < $arrLngth; $i++) {
-    print $wordCount[$i] . "\n";
+    push @argcount, $count;  # store count
+    if ($argcount[$i]) {
+        print $ARGV[$i] . " = " . $argcount[$i] , "\n"; # print value
+    }
+    else {
+        print $ARGV[$i] . " = " . "0\n"; # print if word is not found and value is null
+    }
 }
